c490e6a8c41a94a5d7482c2584c91a1255740664
W45 <73011833+wgs45@users.noreply.github.com>
2025-10-08 19:20:46 +0800

    [Edit] mongodb.ts
    Add new functions

--- src/libs/mongodb.ts
+++ src/libs/mongodb.ts
@@ -1,1 +1,16 @@
 import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGO_URI || "mongodb://admin:password@mongodb:27017";
+const options = {};
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+if (!global._mongoClientPromise) {
+  client = new MongoClient(uri, options);
+  global._mongoClientPromise = client.connect();
+}
+
+clientPromise = global._mongoClientPromise;
+
+export default clientPromise;
